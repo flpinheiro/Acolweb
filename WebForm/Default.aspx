5cbe8e780249a2106121149e202787556115c38b
flpinheiro <flpinheiro@gmail.com>
2019-08-30 18:07:54 -0300

    Initial

---
+++ WebForm/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Tabuada</h1>
+
+    <div>
+        <p>Quantas tabuadas deseja gerar? <asp:TextBox runat="server" ID="Max" /></p>
+        
+        <asp:Button runat="server" ID="Gera" Text="Gerar Tabuadas" />
+        <br />
+        <asp:Label runat="server" ID="Tabuada"></asp:Label>
+        <br />
+    </div>
+
+</asp:Content>
---
+++ WebForm/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int maximun;
+        string ret = "";
+
+        try
+        {
+            maximun = int.Parse(Max.Text);
+        }
+        catch (FormatException)
+        {
+            maximun = 0;
+        }
+        catch (ArgumentNullException)
+        {
+            maximun = 0;
+        }
+        catch (OverflowException)
+        {
+            maximun = 0;
+        }
+
+        if (maximun > 0)
+        {
+            int i = 1;
+            while (i <= maximun)
+            {
+                ret += "<div class='row'>";
+                for (int j = 0; j < 5 && i <= maximun; j++, i++)
+                {
+                    
+                    if (i % 2 == 0)
+                    {   
+                        ret += "<div class='col-2 tabuada'>";
+                    }
+                    else
+                    {
+                        ret += "<div class='col-2 tabuada colored'>";
+                    }
+
+                    for (int k = 0; k < 11; k++)
+                    {
+
+                        ret += $"<p>{i} X {k} = {i * k}</p>";
+                    }
+                    
+                    ret += "</div>";
+                }
+                ret += "</div>";
+            }
+        }
+        Tabuada.Text = ret;
+    }
+}
