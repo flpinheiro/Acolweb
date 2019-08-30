7df68ca4d72eb52124b936ddb5c949293fce30c2
flpinheiro <flpinheiro@gmail.com>
2019-08-30 17:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ 1.WebForm/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1.WebForm.Startup))]
+namespace _1.WebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
